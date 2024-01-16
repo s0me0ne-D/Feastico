9328a6d176b08f49f06cbd1652af910c2950d533
Denys <zdybeldenys@gmail.com>
2024-01-16 15:00:43 +0100

    setup gh-pages

--- src/pages/layout/header/header_components/account/Account.tsx
+++ src/pages/layout/header/header_components/account/Account.tsx
@@ -4,12 +4,19 @@ import { AccountMenu } from "./AccountMenu";
 
 export const Account = () => {
 	const [showMenu, setShowMenu] = useState(false);
+	console.log(showMenu);
 	return (
 		<div
 			className="header_account"
-			onMouseEnter={() => setShowMenu(true)}
+			onMouseEnter={() => {
+				setShowMenu(true);
+				console.log("on");
+			}}
 			onMouseLeave={() => setShowMenu(false)}
-			onClick={() => setShowMenu((prev) => !prev)}
+			onClick={() => {
+				setShowMenu((prev) => !prev);
+				console.log("click");
+			}}
 		>
 			<img src={AccountLogo} alt="account" className="header_account-logo" />
 			{showMenu && <AccountMenu />}
